1c5d8390aca1c57a86f713d8e922b790e16269bb
Tyler Noble <tmnoble11@gmail.com>
2024-07-07 16:15:26 -0400

    modified: schema, in songs changed artists to singular

--- db/migrate/20240703200017_create_songs.rb
+++ db/migrate/20240703200017_create_songs.rb
@@ -4,7 +4,7 @@ class CreateSongs < ActiveRecord::Migration[7.1]
       t.string :name
       t.integer :length
       t.boolean :is_single
-      t.belongs_to :artists, index: true, foreign_key: true
+      t.belongs_to :artist, index: true, foreign_key: true
       
       t.timestamps
     end
